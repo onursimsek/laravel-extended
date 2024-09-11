6d3ee2a74cac0c037edfb7896a0994f95078b5d1
Onur Şimşek <posta@onursimsek.com>
2024-09-11 16:54:58 +0300

    PhpStan fix

--- src/Mixins/StringableMixin.php
+++ src/Mixins/StringableMixin.php
@@ -13,22 +13,25 @@ class StringableMixin
 {
     public function squishBetween(): Closure
     {
-        return fn (string $from, string $to): Stringable => new static(Str::squishBetween($this->value, $from, $to));
+        return function (string $from, string $to): Stringable {
+            /** @phpstan-ignore-next-line */
+            return new static(Str::squishBetween($this->value, $from, $to));
+        };
     }
 
     public function replaceBetweenMatch(): Closure
     {
-        /** @phpstan-ignore-next-line */
-        return fn (string $from, string $to, string $search, string $replace): Stringable => new static(
-            Str::replaceBetweenMatch($this->value, $from, $to, $search, $replace)
-        );
+        return function (string $from, string $to, string $search, string $replace): Stringable {
+            /** @phpstan-ignore-next-line */
+            return new static(Str::replaceBetweenMatch($this->value, $from, $to, $search, $replace));
+        };
     }
 
     public function replaceBetween(): Closure
     {
-        /** @phpstan-ignore-next-line */
-        return fn (string $from, string $to, string $search, string $replace): Stringable => new static(
-            Str::replaceBetween($this->value, $from, $to, $search, $replace)
-        );
+        return function (string $from, string $to, string $search, string $replace): Stringable {
+            /** @phpstan-ignore-next-line */
+            return new static(Str::replaceBetween($this->value, $from, $to, $search, $replace));
+        };
     }
 }
