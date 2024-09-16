466ec3791022299588a12e8098b478b2e6f52fd2
Onur Şimşek <posta@onursimsek.com>
2024-09-16 17:35:57 +0300

    Workbench setup

--- workbench/app/Models/Product.php
+++ workbench/app/Models/Product.php
@@ -2,8 +2,10 @@
 
 namespace Workbench\App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
 }
