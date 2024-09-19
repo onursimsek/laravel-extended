ec6d030b35722fa9660df74e5ca95757ecad7ff9
Onur Şimşek <posta@onursimsek.com>
2024-09-19 21:15:42 +0300

    Fix tests and setup test coverage

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use OnurSimsek\LaravelExtended\Tests\TestCase;
 
-uses(TestCase::class, LazilyRefreshDatabase::class)->in('Unit', 'Feature');
+uses(TestCase::class)->in('Unit', 'Feature');
